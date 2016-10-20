6c7e2e4a94efcbb47d1c3655e982b14e84ef71b3
root <root@ip-172-31-7-147.cn-north-1.compute.internal>
2016-10-20 15:23:15 +0000

    refine dockerfile, build script

--- bridge/build.sh
+++ bridge/build.sh
@@ -2,8 +2,18 @@
 
 set -e
 
-git pull
+if [ -d ./bridge ]; then
+  cd ./bridge && git pull
+else
+  git clone https://github.com/Storj/bridge
+  cd ./bridge
+fi
 
-npm instlal
+npm install
+cd ..
+
+rm -f sj-bridge.tar.gz
+
+tar cfz sj-bridge.tar.gz ./bridge/ 
 
 docker build -t sj-bridge:0.1 .
